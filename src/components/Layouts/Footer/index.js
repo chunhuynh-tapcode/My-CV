15ac919db61e8cab73fd11d8b0f2f5010f85692e
chunhuynh-tapcode <huynhdinhtrung2311@gmail.com>
2025-08-11 16:39:22 +0700

    11/8/2025 Update

--- src/components/Layouts/Footer/index.js
+++ src/components/Layouts/Footer/index.js
@@ -2,52 +2,66 @@ import classNames from "classnames/bind";
 import styles from "./Footer.module.scss";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
-import React from 'react';
-import { useSpring, animated } from '@react-spring/web';
-import { useInView } from 'react-intersection-observer';
+import React from "react";
+import { useSpring, animated } from "@react-spring/web";
+import { useInView } from "react-intersection-observer";
 
 const cx = classNames.bind(styles);
 
 function Footer() {
-    const [ref, inView] = useInView({
-        threshold: 0.2,
-        triggerOnce: true
-    });
-    const fadeInBottomProps = useSpring({
-        from: { opacity: 0, transform: 'translateY(100px)' }, 
-        to: { 
-            opacity: inView ? 1 : 0,
-            transform: inView ? 'translateY(0px)' : 'translateY(100px)' 
-        },     
-        config: { tension: 200, friction: 20 }
-    });
+  const [ref, inView] = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
+  const fadeInBottomProps = useSpring({
+    from: { opacity: 0, transform: "translateY(100px)" },
+    to: {
+      opacity: inView ? 1 : 0,
+      transform: inView ? "translateY(0px)" : "translateY(100px)",
+    },
+    config: { tension: 200, friction: 20 },
+  });
 
-    return ( 
-        <animated.footer ref={ref} style={fadeInBottomProps} className={cx('footer')}>
-            <div className={cx('left')}>
-                <p>CONTACT ME</p>
-            </div>
+  return (
+    <animated.footer
+      ref={ref}
+      style={fadeInBottomProps}
+      className={cx("footer")}
+    >
+      <div className={cx("left")}>
+        <p>CONTACT ME</p>
+      </div>
 
-            <div className={cx('right')}>
-                <div className={cx('phone')}>
-                    <FontAwesomeIcon className={cx('icon')} icon={faPhone} />
-                    <p className={cx('phone-number')}>+84 935038507</p>
-                </div>
-                <a href="https://www.facebook.com/chun.huynh123/" className={cx('social')}>
-                    <FontAwesomeIcon className={cx('icon')} icon={faFacebook} />
-                </a>
-                <a href="https://github.com/chunhuynh-tapcode" className={cx('social')}>
-                    <FontAwesomeIcon className={cx('icon')} icon={faGithub} />
-                </a>
-                <a href="https://www.linkedin.com/in/trung-huynh-a8a668295/" className={cx('social')}>
-                    <FontAwesomeIcon className={cx('icon')} icon={faLinkedin} />
-                </a>
-            </div>
-        </animated.footer>
-    );
+      <div className={cx("right")}>
+        <div className={cx("phone")}>
+          <FontAwesomeIcon className={cx("phone-icon")} icon={faPhone} />
+          <p className={cx("phone-number")}>+84 935038507</p>
+        </div>
+        <a
+          href="https://www.facebook.com/chun.huynh123/"
+          className={cx("social")}
+        >
+          <FontAwesomeIcon className={cx("icon")} icon={faFacebook} />
+        </a>
+        <a href="https://github.com/chunhuynh-tapcode" className={cx("social")}>
+          <FontAwesomeIcon className={cx("icon")} icon={faGithub} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/trung-huynh-a8a668295/"
+          className={cx("social")}
+        >
+          <FontAwesomeIcon className={cx("icon")} icon={faLinkedin} />
+        </a>
+      </div>
+    </animated.footer>
+  );
 }
 
 export default Footer;
